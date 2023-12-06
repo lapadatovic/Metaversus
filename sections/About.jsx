64e38a960c8124b8f7d5bc1552270a0256ffe5be
lapadatovic <lapadatovic.dev@gmail.com>
2023-12-06 16:40:26 +0100

    Added About text and arrow icon for scroll down

--- sections/About.jsx
+++ sections/About.jsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion'
 import styles from '../styles';
 import { TypingText } from '../components'
-import { staggerContainer } from '../utils/motion';
+import { fadeIn, staggerContainer } from '../utils/motion';
 
 // About section
 const About = () => (
@@ -21,6 +21,19 @@ const About = () => (
         title=" | About Metaversus "
         textStyles="text-center"
        />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2,1)}
+        className=' mt-[10px] font-normal sm:text-[30px] text-[20px] text-center text-secondary-white'
+      >
+        <span className='font-extrabold text-white'>Metaverse</span> is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the <span className='font-extrabold text-white'>madness of the metaverse</span> of today, using only <span className='font-extrabold text-white'>VR</span> devices you can easily explore the metaverse world you want, turn your dreams into reality. Let's <span className='font-extrabold text-white'>explore</span> the madness of the metaverse by scrolling down
+      </motion.p>
+      <motion.img 
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src='./arrow-down.svg'
+        alt='arrow_down_icon'
+        className='w-[18px] h-[28px] object-contain mt-[28px]'
+      />
     </motion.div>
   </section>
 );
